c44d46bda345e7ebcb79aeadadb8e7f31ff1ab70
Chenqll <Chenql_716@163.com>
2022-05-05 22:04:11 +0800

    update

--- infer.py
+++ infer.py
@@ -31,7 +31,7 @@ class GeneratorForEager:
         self.tokenizer = BertTokenizer(vocab_file)
     
     def infer(self, sentence):
-        # Encodesssssss
+        # Encodessssssssss
         pdb.set_trace()
         sentence = " ".join([word for word in sentence])
         tokens_list = self.tokenizer.tokenize(sentence)
